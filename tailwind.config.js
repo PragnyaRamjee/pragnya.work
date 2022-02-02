14ff5b3f82a6079819ff5bec14da21c536e08df6
Pragnya Ramjee <pragnya.ramjee@gmail.com>
2022-02-02 19:19:57 +0530

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#212121',
+      'blue': '#0039cb',
+      'grey-dark':'#616161',
+      'grey-light':'#eoeoeo',
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Apercu', 'Inter', 'Arial']
+      }
+    },
+  },
+  variants: {},
+  plugins: []
+}
