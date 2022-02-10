5d195560ab39d07a1eb7a8b51e371628215e4917
Pragnya Ramjee <pragnya.ramjee@gmail.com>
2022-02-10 15:32:02 +0530

    Updated grey and purple colours
    Fixed grey-light and added grey-normal and purple.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
       'black': '#212121',
       'blue': '#0039cb',
       'grey-dark':'#616161',
-      'grey-light':'#eoeoeo',
+      'grey-normal':'#9e9e9e',
+      'grey-light':'#e0e0e0',
+      'purple':'#9333ea',
     },
     extend: {
       fontFamily: {
